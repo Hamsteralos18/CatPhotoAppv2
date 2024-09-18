e034698a06fd0bb98d02fb8d8917f2ab3d3d220f
EmilioYMG <54227209+EmilioYMG@users.noreply.github.com>
2024-09-18 14:53:00 -0600

    18-09-2024

--- app/src/main/java/com/mexiti/catphotoapp/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/mexiti/catphotoapp/ui/screens/HomeScreen.kt
@@ -5,18 +5,28 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
 import com.mexiti.catphotoapp.R
+import com.mexiti.catphotoapp.model.CatPhoto
 import com.mexiti.catphotoapp.viewmodel.CatUiState
 
 
@@ -29,12 +39,62 @@ fun HomeScreen(
                ){
     when( catUiState){
         is CatUiState.Loading -> LoadingScreen(modifier = modifier.fillMaxSize())
-        is CatUiState.Success ->ResultScreen( photos = catUiState.photos,
-            modifier = modifier.fillMaxWidth())
+        is CatUiState.Success -> PhotoGridScreen(photos = catUiState.photos)
         is CatUiState.Error -> ErrorScreen(modifier =  modifier.fillMaxSize())
 
     }
 }
+
+@Composable
+fun PhotoGridScreen (
+    photos: List<CatPhoto>,
+    modifier: Modifier = Modifier,
+    contentPadding: PaddingValues = PaddingValues(0.dp) //espacio entre casillas 0
+){
+    //se adapta el grid al tamaño de imagen
+    LazyVerticalGrid(
+        columns = GridCells.Adaptive(200.dp),
+        modifier = modifier.padding(horizontal = 4.dp),
+        contentPadding = contentPadding
+    ) {
+        items(
+            items = photos,
+            key = {photo -> photo.id}
+        ){
+            photo -> CatPhotoCard(photo = photo, modifier =
+        modifier
+            .padding(4.dp)
+            .fillMaxWidth()
+            .aspectRatio(1.5f)
+            )
+
+        }
+
+
+    }
+
+}
+
+@Composable
+fun CatPhotoCard(
+    photo: CatPhoto,
+    modifier: Modifier,
+
+){
+    AsyncImage(model = ImageRequest.Builder(context = LocalContext.current).data(photo.url)
+        .crossfade(true) //es ver la foto por pedazos hasta que cargue
+        .build(),
+        contentDescription = stringResource(R.string.cat_image),
+        modifier = modifier, //asi se hace mas ancha o mas corta la imagen
+        error = painterResource(id = R.drawable.error_404),
+        placeholder = painterResource(id = R.drawable.carga),
+        contentScale = ContentScale.Crop //ajustar al tamaño de pantalla
+    )
+
+}
+
+
+
 @Composable
 fun LoadingScreen(modifier: Modifier = Modifier){
     Box(modifier = modifier,
@@ -67,6 +127,7 @@ fun ErrorScreen(modifier: Modifier = Modifier){
     }
 }
 
+/*
 @Preview
 @Composable
 fun HomeScreenPreview(){
@@ -77,3 +138,4 @@ fun HomeScreenPreview(){
      }
 
 }
+*/
